f00d3a4e605dbc9310fc5996929995a3fa67c5a2
Raquel Rodrigues <raquel.c.rodrigues@tecnico.ulisboa.pt>
2023-12-06 23:38:41 +0000

    lê ficheiros da diretoria

--- project/p1_base/main.c
+++ project/p1_base/main.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <string.h>
 
 #include "constants.h"
 #include "operations.h"
@@ -10,9 +16,12 @@
 int main(int argc, char *argv[]) {
   unsigned int state_access_delay_ms = STATE_ACCESS_DELAY_MS;
 
-  if (argc > 1) {
+  if (argc < 2) // no directory argument specified
+    return 1;
+
+  if (argc > 2) {
     char *endptr;
-    unsigned long int delay = strtoul(argv[1], &endptr, 10);
+    unsigned long int delay = strtoul(argv[2], &endptr, 10);
 
     if (*endptr != '\0' || delay > UINT_MAX) {
       fprintf(stderr, "Invalid delay value or value too large\n");
@@ -20,6 +29,7 @@ int main(int argc, char *argv[]) {
     }
 
     state_access_delay_ms = (unsigned int)delay;
+
   }
 
   if (ems_init(state_access_delay_ms)) {
@@ -27,17 +37,73 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  while (1) {
+  const char *dir_path = argv[1];
+  size_t dir_path_len = strlen(dir_path);
+  DIR *target_dir = opendir(dir_path);
+  
+  if (target_dir == NULL) {
+    fprintf(stderr, "Failed to open the directory\n");
+    return 1;
+  }
+
+  struct dirent *entry;
+  char *file_path;
+
+  while ((entry = readdir(target_dir)) != NULL) {
     unsigned int event_id, delay;
     size_t num_rows, num_columns, num_coords;
     size_t xs[MAX_RESERVATION_SIZE], ys[MAX_RESERVATION_SIZE];
+    enum Command cmd = 0;
+
+    // Exclude "." and ".." entries
+    if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+        continue;
+    }
+    // allocate space for the file path including the extra slash
+    file_path = (char*)malloc(sizeof(char)*(dir_path_len + strlen(entry->d_name) + 2));
+    
+    if (file_path == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return 1;
+    }
+
+    // Copy the directory path to file_path
+    strcpy(file_path, dir_path);    
+    strcat(file_path, "/");
+    // Concatenate the file name onto file_path
+    strcat(file_path, entry->d_name);
+
+    printf("%s\n", file_path); // REMOVER, PRINT PARA TESTAR
+
+    int fd_jobs = open(file_path, O_RDONLY); //file_path não é const char*, causa problemas?
+
+    if (fd_jobs < 0) {
+      fprintf(stderr, "Error opening file\n");
+      return 1;
+    }
+
+    // Find the position of the dot in the original file name
+    //const char *dotPosition = strrchr(file_path, '.');
+    // Calculate the length of the prefix (characters before the dot)
+    // size_t prefixLength = (size_t) (dotPosition - file_path); //cast manhoso?
+    // char *output_file_path = "";
+    // strncpy(output_file_path, file_path, prefixLength);
+    // strcat(output_file_path, ".out");
+
+    // int fd_out = open(output_file_path, O_CREAT | O_TRUNC | O_WRONLY , S_IRUSR | S_IWUSR);
+
+    // if (fd_out < 0) {
+    //   fprintf(stderr, "Error opening file\n");
+    //   return 1;
+    // }
 
     printf("> ");
     fflush(stdout);
 
-    switch (get_next(STDIN_FILENO)) {
+    while (cmd != EOC) {
+      switch (cmd = get_next(fd_jobs)) {
       case CMD_CREATE:
-        if (parse_create(STDIN_FILENO, &event_id, &num_rows, &num_columns) != 0) {
+        if (parse_create(fd_jobs, &event_id, &num_rows, &num_columns) != 0) {
           fprintf(stderr, "Invalid command. See HELP for usage\n");
           continue;
         }
@@ -49,7 +115,7 @@ int main(int argc, char *argv[]) {
         break;
 
       case CMD_RESERVE:
-        num_coords = parse_reserve(STDIN_FILENO, MAX_RESERVATION_SIZE, &event_id, xs, ys);
+        num_coords = parse_reserve(fd_jobs, MAX_RESERVATION_SIZE, &event_id, xs, ys);
 
         if (num_coords == 0) {
           fprintf(stderr, "Invalid command. See HELP for usage\n");
@@ -63,7 +129,7 @@ int main(int argc, char *argv[]) {
         break;
 
       case CMD_SHOW:
-        if (parse_show(STDIN_FILENO, &event_id) != 0) {
+        if (parse_show(fd_jobs, &event_id) != 0) {
           fprintf(stderr, "Invalid command. See HELP for usage\n");
           continue;
         }
@@ -82,7 +148,7 @@ int main(int argc, char *argv[]) {
         break;
 
       case CMD_WAIT:
-        if (parse_wait(STDIN_FILENO, &delay, NULL) == -1) {  // thread_id is not implemented
+        if (parse_wait(fd_jobs, &delay, NULL) == -1) {  // thread_id is not implemented
           fprintf(stderr, "Invalid command. See HELP for usage\n");
           continue;
         }
@@ -116,8 +182,15 @@ int main(int argc, char *argv[]) {
         break;
 
       case EOC:
-        ems_terminate();
-        return 0;
+        break; //continue?
+      }      
     }
+    close(fd_jobs); //if close != 0 error closing
+      
+    //close(fd_out);
+    free(file_path);    
   }
+  //ems_terminate();
+  closedir(target_dir);
+  return 0;
 }
