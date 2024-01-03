cdc75208f816d20a45b1672ec38529badbb187ee
Raquel Rodrigues <raquel.c.rodrigues@tecnico.ulisboa.pt>
2024-01-03 20:30:27 +0000

    starting threads

--- project2/P2_base/server/main.c
+++ project2/P2_base/server/main.c
@@ -14,12 +14,132 @@
 #include "operations.h"
 #include "eventlist.h"
 #include "requests.h"
+#include "prod_cons_queue.h"
 
 typedef struct {
   int session_id;
   int req_pipe;
   int resp_pipe;
 } Client;
+
+void *consumer_thread_fn(void* queue) {
+  ClientQueue *queue = (ClientQueue*) queue;
+
+  while (1) {
+    Client new_client = dequeue(queue);
+    int quit = 0;
+
+    while (1) {
+      char op_buffer;
+      ssize_t op_bytes_read = read(new_client.req_pipe, &op_buffer, sizeof(char));
+      if (op_bytes_read == 0) {
+        //TODO: clear session_id
+        quit = 1;
+      } else if (op_bytes_read == -1) {
+        fprintf(stderr, "Read failed: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+      } else {
+        int session_id = 0;
+        unsigned int event_id;
+        unsigned int *seats = NULL, *ids = NULL;
+        size_t num_rows, num_cols, num_seats, num_events, num_rc[2];
+        size_t xs[MAX_RESERVATION_SIZE], ys[MAX_RESERVATION_SIZE];
+        int response = 0;
+        switch (op_buffer) {
+          case '1':
+            //FIX ME: various clients
+          case '2':
+            printf("Case 2: QUIT\n");
+            //TODO: clear session_id
+            read_session_id(new_client.req_pipe, &session_id);
+            sessions[session_id] = 0; //mutex?
+            quit = 1;
+            break;
+
+          case '3':
+            printf("Case 3: CREATE\n");
+
+            read_create_request(new_client.req_pipe, &session_id, &event_id, &num_rows, &num_cols);
+
+            if (write_int(new_client.resp_pipe, ems_create(event_id, num_rows, num_cols))) {
+              fprintf(stderr, "Error writing to responses pipe: %s\n", strerror(errno));
+              return 1;
+            }  
+            break;
+
+          case '4':
+            printf("Case 4: RESERVE\n");
+
+            read_reserve_request(new_client.req_pipe, &session_id, &event_id, &num_seats, xs, ys);
+
+            if (write_int(new_client.resp_pipe, ems_reserve(event_id, num_seats, xs, ys))) {
+              fprintf(stderr, "Error writing to responses pipe: %s\n", strerror(errno));
+              return 1;
+            }       
+            break;
+
+          case '5':
+            printf("Case 5: SHOW\n");
+
+            read_show_request(new_client.req_pipe, &session_id, &event_id);
+
+            printf("show | seats address: %p\n", seats);
+
+            response = ems_show(event_id, &num_rows, &num_cols, &seats);
+            
+            if (response) {
+              if (write_int(new_client.resp_pipe, response)) {
+                fprintf(stderr, "Error writing to responses pipe: %s\n", strerror(errno));
+                return 1;
+              }
+            } else {
+              num_rc[0] = num_rows;
+              num_rc[1] = num_cols;
+              printf("show | num_rows: %zu num_cols: %zu seats address: %p\n", num_rc[0], num_rc[1], seats);
+              for (size_t i = 0; i < num_rows*num_cols; i++) {
+                printf("seats[%zu] = %u\n", i, seats[i]);
+              }
+
+              if (write_int(new_client.resp_pipe, response) || write_sizet_array(new_client.resp_pipe, num_rc, 2)
+                  || write_uint_array(new_client.resp_pipe, seats, num_rows*num_cols)) {
+                fprintf(stderr, "Error writing to responses pipe: %s\n", strerror(errno));
+                return 1;
+              }
+            }
+            free(seats);
+            break;
+            
+          case '6':
+            printf("Case 6: LIST\n");
+
+            read_session_id(new_client.req_pipe, &session_id);
+            
+            response = ems_list_events(&num_events, &ids);
+
+            if (response) {
+              if (write_int(new_client.resp_pipe, response)) {
+                fprintf(stderr, "Error writing to responses pipe: %s\n", strerror(errno));
+                return 1;
+              } 
+            } else {
+              if (write_int(new_client.resp_pipe, response) || write_sizet(new_client.resp_pipe, num_events) 
+                  || write_uint_array(new_client.resp_pipe, ids, num_events)) {
+                fprintf(stderr, "Error writing to responses pipe: %s\n", strerror(errno));
+                return 1;
+              }
+
+            }
+            free(ids);
+            break;
+        }
+      }
+      if (quit)
+        break;
+    }
+    close(new_client.req_pipe);
+    close(new_client.resp_pipe);
+  }
+}
 
 int main(int argc, char* argv[]) {
   if (argc < 2 || argc > 3) {
@@ -73,8 +193,10 @@ int main(int argc, char* argv[]) {
   
   // por esta linha dentro do while quando tivermos multiplas sessoes
   Client new_client = {.session_id = 0}; // alterar lógica de criação do session id
+
+  int sessions[MAX_SESSION_COUNT] = {0};
   
-  //while (1) {
+  while (1) {
     //TODO: Read from pipe
     //char buffer[82];
     //ssize_t bytes_read = read(reg_server, buffer, 81);
@@ -159,6 +281,15 @@ int main(int argc, char* argv[]) {
 
       printf("Server opened responses pipe\n");
 
+      // TODO: generate session_id
+      for (int i = 0; i < MAX_SESSION_COUNT; i++) {
+        if (sessions[i] == 0) {
+          new_client.session_id = i;
+          sessions[i] = 1;
+          break;
+        }
+      }
+
       // Respond to the client with the corresponding session id
       if (write_int(new_client.resp_pipe, new_client.session_id)) {
         fprintf(stderr, "Error writing to pipe: %s\n", strerror(errno));
@@ -167,7 +298,8 @@ int main(int argc, char* argv[]) {
     }
 
     //TODO: Write new client to the producer-consumer buffer
-  //}
+  }
+
   int quit = 0;
   while (1) {
     char op_buffer;
@@ -192,7 +324,8 @@ int main(int argc, char* argv[]) {
           printf("Case 2: QUIT\n");
           //TODO: clear session_id
           read_session_id(new_client.req_pipe, &session_id);
-          quit = 1; 
+          sessions[session_id] = 0; //mutex?
+          quit = 1;
           break;
 
         case '3':
@@ -275,6 +408,10 @@ int main(int argc, char* argv[]) {
     if (quit)
       break;
   }
+  close(new_client.req_pipe);
+  close(new_client.resp_pipe);
+
+
   //TODO: Close Server
   close(reg_server);
 
